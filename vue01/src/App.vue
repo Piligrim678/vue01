2343799b873ebefaa3cb3f377ebf9bb281626511
LKabanova <L.Kabanova@russianpost.ru>
2024-12-16 17:19:10 +0300

    1st lesson

--- vue01/src/App.vue
+++ vue01/src/App.vue
@@ -1,9 +1,32 @@
 <script>
+export default {
+  data() {
+    return {
+      info: "Title!!!",
+      anotherInfo: "Very good text",
+    }
+  },
+  methods: {
+    dataHandler(par = "AAAAAAA") {
+      this.info = par
+      this.anotherInfo = "Something new too"
+    }
+  }
+}
 </script>
 
 <template>
+  <h3>{{info}}</h3>
+  <p>{{anotherInfo}}</p>
 
+  <button type="button" @click="dataHandler()">Отправить</button>
 </template>
 
 <style scoped>
+h3 {
+  font-weight: lighter;
+}
+p {
+  color: navy;
+}
 </style>
